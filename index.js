08a0bb4c1d2581c0f59522f1697f62cc65697d32
DeeBest <simphiwedladla8@gmail.com>
2024-04-27 16:17:27 +0200

    added event listeners to the left and right side to enable the active class

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const main = document.querySelector('main');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+left.addEventListener('mouseover', () => {
+  main.classList.add('active-left');
+});
+left.addEventListener('mouseout', () => {
+  main.classList.remove('active-left');
+});
+
+right.addEventListener('mouseover', () => {
+  main.classList.add('active-right');
+});
+right.addEventListener('mouseout', () => {
+  main.classList.remove('active-right');
+});
